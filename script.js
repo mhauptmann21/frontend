181f5768374081ccd4cc17d532277dd7465daf95
Madison <madisonhauptmann@gmail.com>
2025-11-19 10:58:09 -0500

    frontend start

---
+++ script.js
@@ -0,0 +1,12 @@
+// Make an event listener - triggers when the DOM is fully loaded
+addEventListener('DOMContentLoaded', async function() {
+    const response = await fetch('http://localhost:3000/api/songs');
+    const songs = await response.json();
+
+    let html = "";
+    for (let song of songs) {
+        html += `<li>${song.title} - ${song.artist}</li>`;
+    }
+
+    document.querySelector("#addedsong").innerHTML = html;
+});
